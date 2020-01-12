f489586334e1489a99c628f63644e8612c6bfabc
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-01-12 12:05:28 +0100

    add form to send the numbers to calculate

--- index.php
+++ index.php
@@ -5,5 +5,31 @@
    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+    <header>
+        <h2>FACTORIAL CALCULATOR</h2>
+    </header>
+    <article>
+    <form method="post"  >
+<table>
+<tr>
+	<td valign="center">Please give numbers:</td>
+	<td valign="center">
+		<div id="newlink">
+			<div class="feed">
+			   <input type="text" name="myarray[]" value="1" size="20">
+			</div>
+		</div>
+	</td>
+</tr>
+</table>
+	<p>
+		<br>
+		<input type="submit" name="submit1" value="send">
+		
+	</p>
+
+</form>
+
+</article>
 </body>
 </html>
